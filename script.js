a84f7b0b09d7f8cd461831906e906d0012789f21
Giveth2024 <givendarian@gmail.com>
2024-10-07 08:36:10 +0300

    Added responsiveness and more buttons for watching and downloading

---
+++ script.js
@@ -0,0 +1,20 @@
+//Get the id of the iframe
+iframe = document.getElementById("movie_iframe");
+
+function Mega_Function()
+{
+    iframe.setAttribute('src', 'https://mega.nz/embed/KJgiWDBT#9gL_dXM2JxPF_7QVxFep3DmrvPp4td42WKORPodMyNA')
+    iframe.setAttribute('frameborder', '0')
+}
+
+
+function Drive_Function()
+{
+    iframe.setAttribute('src', 'https://drive.google.com/file/d/1WDEjvBOd2_7oj0T4IHrlsui2Fy4r4KLA/preview')
+    iframe.setAttribute('frameborder', '0')
+}
+
+// <iframe frameborder="0" src="https://mega.nz/embed/KJgiWDBT#9gL_dXM2JxPF_7QVxFep3DmrvPp4td42WKORPodMyNA"  ></iframe>
+
+// onclick="window.open('https://drive.google.com/', '_blank')"
+
